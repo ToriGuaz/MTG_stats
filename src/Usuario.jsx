41994a407a2810fbe8d069c9a5a83207ac834562
ToriGuaz <florguazzini92@gmail.com>
2024-10-17 23:44:23 -0300

    cambio estilos

--- src/Usuario.jsx
+++ src/Usuario.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './Usuario.css';
 
 const Usuario = () => {
   const [vida, setVida] = useState(20); // La vida comienza en 20
@@ -12,12 +13,24 @@ const Usuario = () => {
     setVida(vida - 1);
   };
 
+  const aumentarVida5 = () => {
+    setVida(vida + 5);
+  };
+
+  const disminuirVida5 = () => {
+    setVida(vida - 5);
+  };
+
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+    <div className="contenedorUsuario">
       <h1>{nombre}</h1>
       <h2>Vida: {vida}</h2>
-      <button onClick={aumentarVida} style={{ marginRight: '10px' }}>+</button>
-      <button onClick={disminuirVida}>-</button>
+        <div >
+      <button className="buttonStyle" onClick={aumentarVida} >+</button>
+      <button className="buttonStyle" onClick={disminuirVida}>-</button> 
+      <button className="buttonStyle" onClick={aumentarVida5} >+5</button>
+      <button className="buttonStyle" onClick={disminuirVida5} >-5</button>
+        </div>
     </div>
   );
 };
