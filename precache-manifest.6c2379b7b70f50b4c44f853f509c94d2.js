678206625700832eca10a1534a563279baec1a87
ToriGuaz <florguazzini92@gmail.com>
2024-10-22 23:01:28 -0300

    Updates

---
+++ precache-manifest.6c2379b7b70f50b4c44f853f509c94d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e475e5345289800d92316371ba547b33",
+    "url": "/mtg_stats/index.html"
+  },
+  {
+    "revision": "3dc2c3a8dd2c66e46987",
+    "url": "/mtg_stats/static/css/main.c5690472.chunk.css"
+  },
+  {
+    "revision": "3c7b43d55dcdeb786e4a",
+    "url": "/mtg_stats/static/js/2.3d568ae3.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/mtg_stats/static/js/2.3d568ae3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f68e4e48ec7485d7d27",
+    "url": "/mtg_stats/static/js/3.aa90893e.chunk.js"
+  },
+  {
+    "revision": "3dc2c3a8dd2c66e46987",
+    "url": "/mtg_stats/static/js/main.079c7509.chunk.js"
+  },
+  {
+    "revision": "328ea7b080bcaab7ca32",
+    "url": "/mtg_stats/static/js/runtime-main.3cb218f1.js"
+  }
+]);
