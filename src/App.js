cf622527e381e81f00c7588fc82bdafb3a394a06
ToriGuaz <florguazzini92@gmail.com>
2024-10-22 22:56:11 -0300

    creo que lo arregle pero cree otra branch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Crearjugador from './CrearJugador';
 import Usuario from './Usuario';
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <Crearjugador/>
         <Usuario/>
         <Oponente/>
