4d5a75cc4df9751a3f0edca2627012062708846f
ENPera1994 <enpera1994@gmail.com>
2024-11-22 16:44:03 -0300

    movido boton hamburguesa a app.js y hecha la logica para cargarlo solo al clickear dicho boton

--- src/App.js
+++ src/App.js
@@ -5,15 +5,36 @@ import './Styles/app.css';
 
 function App() {
   const [gameID, setGameID] = useState('');
+  const [isLandingPageVisible, setIsLandingPageVisible] = useState(false);
 
   const handleSetGameID = (id) => {
     setGameID(id);
+    setIsLandingPageVisible(false); 
+  };
+
+  const toggleLandingPage = () => {
+    setIsLandingPageVisible((prev) => !prev);
+    console.log(isLandingPageVisible);
   };
 
   return (
     <div>
-      <LandingPage  onGameSelect={handleSetGameID} />
-      <Game  gameID={gameID} />
+      <label className="hamburger">
+        <input type="checkbox" checked={isLandingPageVisible} onChange={toggleLandingPage} />
+        <svg viewBox="0 0 32 32">
+          <path
+            className="line line-top-bottom"
+            d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
+          ></path>
+          <path className="line" d="M7 16 27 16"></path>
+        </svg>
+      </label>
+  
+      {isLandingPageVisible && (
+        <LandingPage onGameSelect={handleSetGameID} />
+      )}
+  
+      <Game gameID={gameID} />
     </div>
   );
 }
